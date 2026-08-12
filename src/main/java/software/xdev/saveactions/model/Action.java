b70b5fd86dc382255d30c348d7cb0389c259eaac
zujiInk <inkzuji@gmail.com>
2026-08-12 16:32:33 +0800

    Translate action descriptions into Chinese

--- src/main/java/software/xdev/saveactions/model/Action.java
+++ src/main/java/software/xdev/saveactions/model/Action.java
@@ -15,100 +15,100 @@ import java.util.stream.Stream;
 public enum Action
 {
 	// Activation
-	activate("Activate save actions on save (before saving each file, performs the configured actions below)",
+	activate("启用保存时执行操作（保存每个文件前执行下方已配置的操作）",
 		activation, true),
 	
-	activateOnShortcut("Activate save actions on shortcut (default \"CTRL + SHIFT + S\")",
+	activateOnShortcut("启用快捷键执行保存操作（默认“CTRL + SHIFT + S”）",
 		activation, false),
 	
-	activateOnBatch("Activate save actions on batch (\"Code > Save Actions > Execute on multiple files\")",
+	activateOnBatch("启用批量执行保存操作（“代码 > Save Actions > 在多个文件上执行”）",
 		activation, false),
 	
-	noActionIfCompileErrors("No action if compile errors (applied per file)",
+	noActionIfCompileErrors("文件存在编译错误时不执行操作（逐个文件判断）",
 		activation, false),
 	
-	processAsync("Process files asynchronously "
-		+ "(will result in less UI hangs but may break if a processor needs the UI)",
+	processAsync("异步处理文件（可减少界面卡顿，但依赖界面的处理器可能无法正常工作）",
 		activation, false),
 	
 	// Global
-	organizeImports("Optimize imports",
+	organizeImports("优化导入",
 		global, true),
 	
-	reformat("Reformat file",
+	reformat("重新格式化文件",
 		global, true),
 	
-	reformatChangedCode("Reformat only changed code (only if VCS configured)",
+	reformatChangedCode("仅重新格式化已更改的代码（仅在配置版本控制时可用）",
 		global, false),
 	
-	rearrange("Rearrange fields and methods "
-		+ "(configured in \"File > Settings > Editor > Code Style > (...) > Arrangement\")",
+	rearrange("重新排列字段和方法"
+		+ "（在“文件 > 设置 > 编辑器 > 代码样式 > (...) > 排列”中配置）",
 		global, false),
 	
 	// Build
-	compile("[experimental] Compile files (using \"Build > Build Project\")",
+	compile("[实验性] 编译文件（使用“构建 > 构建项目”）",
 		build, false),
 	
-	reload("[experimental] Reload files in running debugger (using \"Run > Reload Changed Classes\")",
+	reload("[实验性] 在运行中的调试器里重新加载文件"
+		+ "（使用“运行 > 重新加载已更改的类”）",
 		build, false),
 	
-	executeAction("[experimental] Execute an action (using quick lists at "
-		+ "\"File > Settings > Appearance & Behavior > Quick Lists\")",
+	executeAction("[实验性] 执行操作"
+		+ "（使用“文件 > 设置 > 外观与行为 > 快速列表”中的快速列表）",
 		build, false),
 	
 	// Java fixes
-	fieldCanBeFinal("Add final modifier to field",
+	fieldCanBeFinal("为字段添加 final 修饰符",
 		java, false),
 	
-	localCanBeFinal("Add final modifier to local variable or parameter",
+	localCanBeFinal("为局部变量或参数添加 final 修饰符",
 		java, false),
 	
-	localCanBeFinalExceptImplicit("Add final modifier to local variable or parameter except if it is implicit",
+	localCanBeFinalExceptImplicit("为非隐式局部变量或参数添加 final 修饰符",
 		java, false),
 	
-	methodMayBeStatic("Add static modifier to methods",
+	methodMayBeStatic("为可静态化的方法添加 static 修饰符",
 		java, false),
 	
-	unqualifiedFieldAccess("Add this to field access",
+	unqualifiedFieldAccess("为字段访问添加 this 限定符",
 		java, false),
 	
-	unqualifiedMethodAccess("Add this to method access",
+	unqualifiedMethodAccess("为方法调用添加 this 限定符",
 		java, false),
 	
-	unqualifiedStaticMemberAccess("Add class qualifier to static member access",
+	unqualifiedStaticMemberAccess("为静态成员访问添加类限定符",
 		java, false),
 	
-	customUnqualifiedStaticMemberAccess("Add class qualifier to static member access outside declaring class",
+	customUnqualifiedStaticMemberAccess("为声明类之外的静态成员访问添加类限定符",
 		java, false),
 	
-	missingOverrideAnnotation("Add missing @Override annotations",
+	missingOverrideAnnotation("添加缺失的 @Override 注解",
 		java, false),
 	
-	useBlocks("Add blocks to if/while/for statements",
+	useBlocks("为 if/while/for 语句添加代码块",
 		java, false),
 	
-	generateSerialVersionUID("Add a serialVersionUID field for Serializable classes",
+	generateSerialVersionUID("为 Serializable 类添加 serialVersionUID 字段",
 		java, false),
 	
-	unnecessaryThis("Remove unnecessary this to field and method",
+	unnecessaryThis("删除字段和方法访问中不必要的 this 限定符",
 		java, false),
 	
-	finalPrivateMethod("Remove final from private method",
+	finalPrivateMethod("删除私有方法的 final 修饰符",
 		java, false),
 	
-	unnecessaryFinalOnLocalVariableOrParameter("Remove unnecessary final for local variable or parameter",
+	unnecessaryFinalOnLocalVariableOrParameter("删除局部变量或参数不必要的 final 修饰符",
 		java, false),
 	
-	explicitTypeCanBeDiamond("Remove explicit generic type for diamond",
+	explicitTypeCanBeDiamond("删除可由菱形操作符推断的显式泛型类型",
 		java, false),
 	
-	unnecessarySemicolon("Remove unnecessary semicolon",
+	unnecessarySemicolon("删除不必要的分号",
 		java, false),
 	
-	singleStatementInBlock("Remove blocks from if/while/for statements",
+	singleStatementInBlock("删除 if/while/for 单语句代码块",
 		java, false),
 	
-	accessCanBeTightened("Change visibility of field or method to lower access",
+	accessCanBeTightened("降低字段或方法的访问级别",
 		java, false);
 	
 	private final String text;
